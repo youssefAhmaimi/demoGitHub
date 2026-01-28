2fe011e953a95204432a3f695c982da3f1eb2a16
youssefAhmaimi <youssefahm.pro@gmail.com>
2026-01-28 09:29:45 +0100

    Q9.

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,45 @@
 #include <Arduino.h>
 
-char broche = 26; // Broche de la LED intégrée de l'ESP32
 
-void tachePeriodique(void *pvParameters)
+
+void maTache(void *parametres)
 {
-  TickType_t xLastWakeTime;
-  double x = 0, y = 0;
-
-  // Lecture du nombre de ticks quand la tâche débute
-  xLastWakeTime = xTaskGetTickCount();
-  while (1)
+  int v1 = 0;
+  static int v2 = 0;
+  while (1) // boucle infinie
   {
-    digitalWrite(broche, HIGH);    
-    TickType_t debCalcul = xTaskGetTickCount();
-    // Des calculs pour que la tâche occupe le processeur
-    int nbTour = 3000 + rand() % 3000;
-    
-    for (int i = 0; i < nbTour; i++)
-    {
-      double xn = sin(x) + cos(y);
-      double yn = cos(x) + sin(y);
-      double d = sqrt(xn * xn + yn * yn);
-      if (d == 0)
-      {
-        x = 0;
-        y = 0;
-      }
-      else
-      {
-        x = xn / d;
-        y = yn / d;
-      }
-    }
-    TickType_t finCalcul = xTaskGetTickCount();
-    Serial.printf("Temps de calcul = %u\n", finCalcul - debCalcul);
-    // Endort la tâche pendant le temps restant par rapport au réveil,
-    // ici 200ms, donc la tâche s'effectue toutes les 200ms
-    digitalWrite(broche, LOW);
-//    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(200)); // toutes les 200 ms
-    vTaskDelay(pdMS_TO_TICKS(200)); // toutes les 200 ms
+    Serial.printf("%s : v1=%d v2=%d\n", pcTaskGetName(NULL), v1, v2);
+    v1++;
+    v2++;
+    delay(500);
   }
 }
+
+void maTache2(void *parametres)
+{
+  int v1 = 4;
+  static int v3 = 4;
+  while (1) // boucle infinie
+  {
+    Serial.printf("%s : v1=%d v3=%d\n", pcTaskGetName(NULL), v1, v3);
+    v1++;
+    v3++;
+    delay(500);
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
   Serial.printf("Initialisation\n");
 
-  pinMode(broche, OUTPUT);
-  // Création de la tâche périodique
-  xTaskCreate(tachePeriodique, "Tâche périodique", 10000, NULL, 2, NULL);
+  // Création de la tâche maTache
+  xTaskCreate(maTache2, "maTache2", 10000, NULL, 2, NULL);
+  xTaskCreate(maTache, "maTache", 10000, NULL, 2, NULL);
+  
 }
+
+
 void loop()
 {
   static int i = 0;
